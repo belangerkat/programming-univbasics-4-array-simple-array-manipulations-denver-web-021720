10990a219ffc9cb0baa31b17afdcdc764bebb5bf
Katie Belanger <belangerkat@gmail.com>
2020-02-07 19:32:24 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,43 @@
+#result of method calls on arrays?
+#simple array manipulations
+
+#method - 2 arguments- array, string, adds string to end of array using .push
+def using_push(array, string)
+
+#method - 2 arguments- array, string adds string to front using .unshift
+def using_unshift(array, string)
+
+
+#pop-method-arg-array-remove last element and return that element
+def using_pop(array)
+
+
+#arg-array .pop with arg of 2 to remove last 2 array items and return them
+def pop_with_args(array)
+
+
+#arg-array uses .shift to remove first item and return it
+def using_shift(array)
+
+
+#arg-array uses .shift wiht arg of 2 to remove and return 1st 2 items from array
+def shift_with_args(array)
+
+
+def using_concat(arr1, arr2)
+  .concat
+
+def using_insert(array, element)
+  .insert
+
+def using_uniq(array)
+  .uniq
+
+
+def using_flatten(array)
+  .flatten
+
+def using_delete(array, string)
+  .delete
+
+def using_delete_at(array, Integer)
